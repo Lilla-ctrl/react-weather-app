afb1d034ba2fc1f36f984bc6bbfd23120a53f6f9
Lilla-ctrl <berenyi.lilu@gmail.com>
2023-11-14 11:59:09 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Coded by Lilla Berényi,{" "}
-        <a
-          href="https://github.com/Lilla-ctrl/react-weather-app"
-          target="_blank"
-        >
-          open-source code
-        </a>{" "}
-        on GitHub.
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          Coded by Lilla Berényi,{" "}
+          <a
+            href="https://github.com/Lilla-ctrl/react-weather-app"
+            target="_blank"
+          >
+            open-source code
+          </a>{" "}
+          on GitHub.
+        </footer>
+      </div>
     </div>
   );
 }
