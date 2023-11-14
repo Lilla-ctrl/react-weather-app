8516fc673283056c90d89e8a2f901435320857dd
Lilla-ctrl <berenyi.lilu@gmail.com>
2023-11-14 11:45:40 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        Coded by Lilla Berényi,{" "}
+        <a
+          href="https://github.com/Lilla-ctrl/react-weather-app"
+          target="_blank"
+        >
+          open-source code
+        </a>{" "}
+        on GitHub.
+      </footer>
+    </div>
+  );
 }
