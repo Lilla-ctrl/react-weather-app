47ea2513b7ba9dfa1353eb13e290acff5b7daa84
Lilla-ctrl <berenyi.lilu@gmail.com>
2024-12-15 17:01:03 +0100

    updates to forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,17 +1,39 @@
+import { useState } from "react";
+import axios from "axios";
 import React from "react";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 
-export default function WeatherForecast() {
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <WeatherForecastDay />
-        <WeatherForecastDay />
-        <WeatherForecastDay />
-        <WeatherForecastDay />
-        <WeatherForecastDay />
+export default function WeatherForecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecastWeatherData, setForecastWeatherData] = useState(null);
+
+  function search() {
+    let apiKey = "3et61975bb6d4a4foabfddbded4a0a8e";
+    let city = props.data.city;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleForecastResponse);
+  }
+  function handleForecastResponse(response) {
+    setForecastWeatherData({
+      ready: true,
+      tempmin: response.data.daily[0].temperature.minimum,
+      tempmax: response.data.daily[0].temperature.maximum,
+      date: new Date(response.data.daily[0].time * 1000),
+    });
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <WeatherForecastDay data={forecastWeatherData} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return null;
+  }
 }
--- src/WeatherForecastDay.jsx
+++ src/WeatherForecastDay.jsx
@@ -1,11 +1,11 @@
-export default function WeatherForecastDay() {
-  return (
-    <div className="col">
+export default function WeatherForecastDay(props) {
+  <div className="col">
       <div className="Forecast-day">Thu</div> Icon{" "}
       <div className="Forecast-temperature">
-        <span className="Forecast-temperature-max">19°</span>{" "}
-        <span className="Forecast-temperature-min">10°</span>
+        <span className="Forecast-temperature-max">
+         {props.data.tempmax}
+        </span>{" "}
+        <span className="Forecast-temperature-min">{props.data.tempmin}</span>
       </div>
     </div>
-  );
 }
