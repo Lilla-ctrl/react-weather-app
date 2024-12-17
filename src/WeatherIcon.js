e60a87510090767fc6b0f5a6493622d7b86c569e
Lilla-ctrl <berenyi.lilu@gmail.com>
2024-12-17 17:39:02 +0100

    Update WeatherIcon.js

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -3,5 +3,5 @@ import React from "react";
 export default function WeatherIcon(props) {
   const imgsrc = `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${props.code}.png`;
 
-  return <img src={imgsrc} />;
+  return <img src={imgsrc} alt="Weather icon" />;
 }
