bad3ec0b2a8ace4212a99be164b35959d0d0f952
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-06-11 20:47:07 +0200

    using moment.js for the date and time

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,40 +1,5 @@
-import React from "react";
+import moment from "moment";
 
 export default function FormattedDate(props) {
-
-  let date = new Date(props.date * 1000);
-
-  function formatDay() {
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-
-  let day = date.getDay();
-
-  return days[day];
-
+  return <div>{moment(props.date).format("dddd HH:mm")}</div>;
 }
-  let hours = props.date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = props.date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  return (
-    <div>
-      {formatDay()}, {hours}:{minutes}
-    </div>
-  );
-}
-
-
-
